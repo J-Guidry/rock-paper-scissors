7c0e97e600bf626cfdc0ec174babf19ed9fe7adb
Guidry <jh_guidry@outlook.com>
2019-09-03 16:34:15 -0500

    listeners removed, handler methods created that call on functions yet defined

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ const UI = {
 }
 
 const handlers = {
-    setEventListeners: function(){
-       const messageBox = document.querySelector("#message");
-       const rounds = document.querySelector("#rounds");
-       const wins = document.querySelector("#wins");
-       const losses = document.querySelector("#losses");
-       const ties = document.querySelector("#ties");
+    chooseRock: function(){
+        const rock = 0;
+        let message = data.playRound(rock);
+        data.game(message);
+    },
+    choosePaper: function(){
+        const paper = 1;
+        let message =data.playRound(paper);
+        data.game(message);
+    },
+    chooseScissors: function(){
+        const scissors = 2;
+        let message = data.playRound(scissors);
+        data.game(message)
+    },
+    checkRounds: function(){
+        let check = data.checkRounds();
 
-    },
-    chooseRock: function(event){
-        console.log("hi");
-    },
-    init: function(){
-        this.setEventListeners();
     }
 }
 
