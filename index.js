f4f6c5fc58916d599594832c32c7dc5328ebbd2b
Guidry <jh_guidry@outlook.com>
2019-09-02 20:24:02 -0500

    set up query selectors

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-data = {
+const data = {
 
 }
 
-UI = {
+const UI = {
 
 }
 
-handlers = {
-    
+const handlers = {
+    setEventListeners: function(){
+       const messageBox = document.querySelector("#message");
+       const rounds = document.querySelector("#rounds");
+       const wins = document.querySelector("#wins");
+       const losses = document.querySelector("#losses");
+       const ties = document.querySelector("#ties");
+       const rock = document.querySelector("#rock");
+       const paper = document.querySelector("#paper");
+       const scissors = document.querySelector("#scissors");
+    }
 }
+
