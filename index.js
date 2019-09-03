a4bd1c9a53b48a93a48f87de87c3513ebede354a
Guidry <jh_guidry@outlook.com>
2019-09-03 16:37:50 -0500

    init removed, anchor tags changed to buttons, less width on choices div

--- index.js
+++ index.js
@@ -9,23 +9,27 @@ const UI = {
 const handlers = {
     chooseRock: function(){
         const rock = 0;
-        let message = data.playRound(rock);
-        data.game(message);
+        let message = data.game(rock);
+        UI.update(message);
+        this.checkRounds()
     },
     choosePaper: function(){
         const paper = 1;
-        let message =data.playRound(paper);
-        data.game(message);
+        let message = data.game(paper);
+        UI.update(message);
+        this.checkRounds();
     },
     chooseScissors: function(){
         const scissors = 2;
-        let message = data.playRound(scissors);
-        data.game(message)
+        let message = data.game(scissors);
+        UI.update(message);
+        this.checkRounds();
     },
     checkRounds: function(){
         let check = data.checkRounds();
-
+        if (check === true){
+            data.endGame();
+            UI.endgame();
+        }
     }
 }
-
-handlers.init()
